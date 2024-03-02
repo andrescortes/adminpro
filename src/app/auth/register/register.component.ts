9c62a4adf5895191c70bee44f68ee3e29e03ac06
Miguel Cortes <miguel.cortes@ias.com.co>
2024-03-01 21:46:46 -0500

    added content and styles css

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: ``
+  styleUrls: [
+    './register.component.css'
+  ]
 })
 export class RegisterComponent {
 
